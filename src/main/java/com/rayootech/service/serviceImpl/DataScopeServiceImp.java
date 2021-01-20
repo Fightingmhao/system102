2e7d5716d9b4791fef5a6542ee86076b9f0be7fc
fightingHao <fightingmhao@163.com>
2021-01-20 17:32:53 +0800

    第一次上传项目

---
+++ src/main/java/com/rayootech/service/serviceImpl/DataScopeServiceImp.java
@@ -0,0 +1,45 @@
+package com.rayootech.service.serviceImpl;
+
+import com.rayootech.mapper.UserDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class DataScopeServiceImp {
+    @Autowired
+    private UserDtoMapper userDtoMapper;
+
+    public String getDataScope(String canHremail) {
+        //1、查询数据权限  1、全部数据权限，2、部门及以下，3、个人数据权限
+        String scope = userDtoMapper.getDataScope(canHremail).get(0);
+        switch (scope) {
+            case "1":
+                return null;
+            case "2":
+                List<String> list = userDtoMapper.getUserDataScope(canHremail);
+                String usernameList = "";
+                for (int i = 0; i < list.size(); i++) {
+                    usernameList += ("'" + list.get(i) + "'");
+                    if (i < list.size() - 1) {
+                        usernameList += ",";
+                    }
+                }
+                return usernameList;
+            default:
+                return "'" + canHremail + "'";
+        }
+    }
+
+    public String getDeptype(String uname) {
+        return userDtoMapper.getDeptype(uname);
+    }
+
+    public String getDeptName(String uname){
+        return userDtoMapper.selectDeptNameByUserName(uname);
+    }
+
+    public String getDeptypeById(Integer deptId) {
+        return userDtoMapper.getDeptypeByID(deptId);
+    }
+}
